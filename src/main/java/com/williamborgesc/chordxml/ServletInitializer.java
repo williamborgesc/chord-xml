80ec67fe60f546d0ecf767f9f7a3477ee8e22eed
Will Correia <williamborgesc@gmail.com>
2020-11-23 18:23:28 -0300

    Making project a war and adding index page

---
+++ src/main/java/com/williamborgesc/chordxml/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.williamborgesc.chordxml;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChordXmApplication.class);
+    }
+}
+
